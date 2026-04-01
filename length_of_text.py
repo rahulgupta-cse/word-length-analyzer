fe70b4375e1f0a3e3a1bdfa8d548f1b0cde21e94
Rahul Gupta <toorahulgupta@gmail.com>
2026-04-02 01:42:12 +0530

    added logics to improve continuous input handling

--- length_of_text.py
+++ length_of_text.py
@@ -1,5 +1,10 @@
 #Program to find length of each word from line of Text
 
+
+#import sys for exit out of program
+import sys 
+
+
 #Defing the function
 def wordlen(lineoftext):
     #logic to find the length
@@ -10,6 +15,12 @@ def wordlen(lineoftext):
     print("-" * 50) #decoration part
     print("Total length of Text with space: =",len(lineoftext))
     print("-" * 50)
+
+    #Logic here to stop or continue the Program
+    tryagain = input("Due Your want to  Try again for another line of text Y/N:").lower()
+    if tryagain == 'n':
+        print("Program Exited Successfully")
+        sys.exit()
 
 #main Program
 while True:
